f496a80a098c9de84af002ed9e9603371b2c9cb8
Jean <jeanarby030108@gmail.com>
2025-01-24 19:09:34 +0700

    lagi1

--- js/include.js
+++ js/include.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Load Header
-    fetch('../includes/header.html')
+    fetch('/includes/header.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('header-placeholder').innerHTML = data;
